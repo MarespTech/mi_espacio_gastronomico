754a07bfca6aeb49c2fefab4fc46283bf2064d93
Martin Espericueta <martin.m.espericueta@gmail.com>
2021-02-14 15:50:12 -0800

    add console log

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,12 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
+console.log(`BD nombre: ${process.env.DB_NOMBRE}`);
+console.log(`Usuario: ${process.env.DB_USER}`);
+console.log(`Password: ${process.env.DB_PASS}`);
+console.log(`BD Host: ${process.env.DB_HOST}`);
+console.log(`BD PORT: ${process.env.DB_PORT}`);
+
 const db = new Sequelize(process.env.DB_NOMBRE, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
